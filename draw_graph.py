d73ab3a76370effed8b1b25ba6ce8e7944c7e678
bataevk <btaev.i@gmail.com>
2025-06-03 20:00:54 +0300

    RE-structed

--- draw_graph.py
+++
@@ -1,8 +0,0 @@
-from IPython.display import Image, display
-
-def draw_graph(graph):
-    try:
-        display(Image(graph.get_graph().draw_mermaid_png()))
-    except Exception:
-        # This requires some extra dependencies and is optional
-        pass
